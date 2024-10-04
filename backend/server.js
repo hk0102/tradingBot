2a27b52b08c830023d5258eafd249eb87821be8a
Harpreet Kaur <harkhush1102@gmail.com>
2024-10-04 17:08:58 +0530

    added environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require('http');
 const socketIo  = require('socket.io');
-
+const dotenv = require('dotenv')
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io =  socketIo(server);
 
-const PORT = 7700;
+dotenv.config();
+app.get("/*", (req, res) => {
+    res.json({message: "Welcome to the Trading Bot"});
+  });
+
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
-    console.log(`Trading bot running on port ${PORT}`);
+    console.log(`Trading bot listening on port ${PORT}`);
 });
